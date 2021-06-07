8b7e4ba50c055a5d158a0a3a5dc41001f92cf048
zero <lidm@ziroom.com>
2021-06-07 17:51:02 +0800

    gateway 修复版本问题

--- cloud-gateway-samples/src/main/java/com/example/demo/MyGatewayFilterFactory.java
+++ cloud-gateway-samples/src/main/java/com/example/demo/MyGatewayFilterFactory.java
@@ -2,13 +2,14 @@ package com.example.demo;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Component
-public class MyGatewayFilterFactory extends AbstractGatewayFilterFactory<MyGatewayFilterFactory.Config> {
+public class MyGatewayFilterFactory extends AbstractGatewayFilterFactory<MyGatewayFilterFactory.Config> implements Ordered {
 
     public MyGatewayFilterFactory() {
         super(Config.class);
@@ -25,6 +26,11 @@ public class MyGatewayFilterFactory extends AbstractGatewayFilterFactory<MyGatew
             System.out.println("My Filter ....");
             return chain.filter(exchange);
         };
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
     }
 
     public static class Config {
